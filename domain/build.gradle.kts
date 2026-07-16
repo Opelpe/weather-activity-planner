8970491ce7c0f6184c5c59c472ef5302388ae135
Opelpe <91432013+Opelpe@users.noreply.github.com>
2026-07-16 23:16:27 +0200

    Add ConnectivityRepository interface to domain

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -15,6 +15,7 @@ kotlin {
 
 dependencies {
     implementation(libs.javax.inject)
+    implementation(libs.kotlinx.coroutines.core)
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
