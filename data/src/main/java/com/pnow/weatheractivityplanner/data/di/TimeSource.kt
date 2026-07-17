324e26092fafbe523bc2f42b2382f3114b2735a3
Opelpe <91432013+Opelpe@users.noreply.github.com>
2026-07-17 21:51:36 +0200

    Split TimeModule out of TimeSource.kt to match the DispatcherQualifiers/CoroutineModule convention

--- data/src/main/java/com/pnow/weatheractivityplanner/data/di/TimeSource.kt
+++ data/src/main/java/com/pnow/weatheractivityplanner/data/di/TimeSource.kt
@@ -1,20 +1,6 @@
 package com.pnow.weatheractivityplanner.data.di
 
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
-import javax.inject.Singleton
-
-@Module
-@InstallIn(SingletonComponent::class)
-abstract class TimeModule {
-
-    @Binds
-    @Singleton
-    internal abstract fun bindTimeSource(impl: TimeSourceImpl): TimeSource
-}
 
 internal fun interface TimeSource {
 
