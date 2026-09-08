026e1722f07838f28b8f303c2373c538dbd6d44b
Opelpe <91432013+Opelpe@users.noreply.github.com>
2026-09-09 01:52:52 +0200

    Bump versionName to 1.1.0 and versionCode to 10100

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = AppConfig.APPLICATION_ID
         minSdk = 29
         targetSdk = 37
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 10100
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "com.google.dagger.hilt.android.testing.HiltTestRunner"
     }
